dcb674b15c6551ee4efab7a6b5d6efe206a47a1a
KelvinErlangga <kelvinerlanggaa@gmail.com>
2024-08-16 15:37:16 +0700

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import "elements/Button"
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>LandingPage</div>
+    )
+  }
+}
